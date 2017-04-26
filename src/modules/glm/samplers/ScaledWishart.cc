fe85d31fdc46afb7ca8293098dce2d19bfbcc256
martyn_plummer <devnull@localhost>
2017-04-26 15:29:03 +0200

    Testing more efficient version of sampleWishart for glm module.

--- src/modules/glm/samplers/ScaledWishart.cc
+++ src/modules/glm/samplers/ScaledWishart.cc
@@ -25,12 +25,25 @@ using std::copy;
 
 #define F77_DPOTRF F77_FUNC(dpotrf,DPOTRF)
 #define F77_DPOTRI F77_FUNC(dpotri, DPOTRI)
+#define F77_DTRTRI F77_FUNC(dtrtri, DTRTRI)
+#define F77_DTRMM  F77_FUNC(dtrmm, DTRMM)
+#define F77_DSYRK  F77_FUNC(dsyrk, DSYRK)
 
 extern "C" {
     void F77_DPOTRF (const char *uplo, const int *n, double *a,
 		     const int *lda, const int *info);
     void F77_DPOTRI (const char *uplo, const int *n, double *a,
 		     const int *lda, const int *info);
+    void F77_DTRTRI (const char *uplo, const char *diag,
+		     const int *n, double *a, const int *lda, const int *info);
+    void F77_DTRMM(const char *side, const char *uplo, const char *transa,
+		   const char *diag, const int *m, const int *n,
+		   const double *alpha, const double *a, const int *lda,
+		   double *b, const int *ldb);
+    void F77_DSYRK(const char *uplo, const char *trans, const int *n,
+		   const int *k,
+		   const double *alpha, const double *a, const int *lda,
+		   const double *beta, double *c, const int *ldc);
 }
 
 
@@ -143,6 +156,140 @@ static void sampleWishart(double *x, int length,
 	}
     }
     delete [] Ztrans;
+}
+
+
+//DEBUGGIN: Alternate version of sampleWishart using BLAS calls
+//and avoiding double Cholesky decomposition
+static void sampleWishart2(double *x, int length,
+			   double const *R, double k, int nrow,
+			   jags::RNG *rng)
+{
+    int info = 0;
+    /* 
+       Generate random Wishart variable, using an algorithm proposed
+       by Bill Venables and originally implemented in S.
+    */
+
+    if (length != nrow*nrow) {
+	jags::throwLogicError("invalid length in DWish::randomSample");
+    }
+
+    /* 
+       Get inverse of R using the Cholesky decomposition (dpotrf) and
+       then inverting the upper triangular factor (dtrtri).
+    */
+    vector<double> C(length);
+
+    copy(R, R + length, C.begin());
+    F77_DPOTRF("U", &nrow, &C[0], &nrow, &info);
+    if (info != 0) {
+	jags::throwRuntimeError("Failed to get Cholesky decomposition of R");
+    }
+    F77_DTRTRI("U", "N", &nrow, &C[0], &nrow, &info);
+    if (info != 0) {
+	jags::throwRuntimeError("Failed to invert Cholesky decomposition of R");
+    }
+
+    /*********** DEBUGGIN **********/
+    /* 
+       Get inverse of R. Venables' algorithm was implemented in
+       terms of the inverse of R, but we use a different parameterization
+       to preserve conjugacy.
+    */
+    double * C2 = new double[length];
+    copy(R, R + length, C2);
+    if(!inverse_spd(C2, nrow)) {
+	jags::throwRuntimeError("Inverse failed in DWish::randomSample");
+    }
+    /* Get Choleskly decomposition of C2 */
+    F77_DPOTRF("U", &nrow, C2, &nrow, &info);
+    if (info != 0) {
+	jags::throwRuntimeError("Failed to get Cholesky decomposition of R");
+    }
+
+    /* Set lower triangle of C2 to zero */
+    for (int j = 0; j < nrow; j++) {
+	double * C2_j = &C2[j*nrow]; //column j of matrix C2
+	for (int i = j + 1; i < nrow; i++) {
+	    C2_j[i] = 0;
+	}
+    }
+
+    /* Check C vs C2 (upper triangle) */
+    for (int j = 0; j < nrow; j++) {
+	double * C_j = &C[j*nrow]; //column j of matrix C
+	double * C2_j = &C2[j*nrow]; //column j of matrix C2
+	for (int i = 0; i < j; ++i) {
+	    if (abs(C_j[i] - C2_j[i]) > 1e-6)
+		jags::throwLogicError("DEBUG: sampleWishart mismatch 1");
+	}
+    }
+
+    /********** END DEBUGGIN **********/
+    
+    /* Generate square root of Wishart random variable:
+       - diagonal elements are square root of Chi square
+       - upper off-diagonal elements are normal
+       - lower off-diagonal elements are zero
+    */
+    vector<double> Z(length);
+    for (int j = 0; j < nrow; j++) {
+	double *Z_j = &Z[j*nrow]; //jth column of Z
+	for (int i = 0; i < j; i++) {
+	    Z_j[i] = rnorm(0, 1, rng);
+	}
+	Z_j[j] = sqrt(rchisq(k - j, rng));    
+	for (int i = j + 1; i < nrow; i++) {
+	    Z_j[i] = 0;
+	}
+    }
+
+    /**************** DEBUGGIN **************/
+    /* Transform Z with Cholesky decomposition */
+    vector<double> Ztrans(length);
+    for (int i = 0; i < nrow; i++) {
+	for (int j = 0; j < nrow; j++) {
+	    double zz = 0;
+	    for (int l = 0; l < nrow; l++) {
+		zz += Z[nrow * l + i] * C2[nrow * j + l];
+	    }
+	    Ztrans[nrow * j + i] = zz;
+	}
+    }
+    /*********** END DEBUGGIN **********/
+    
+    // Z = Z %*% C 
+    double one = 1;
+    F77_DTRMM("R", "U", "N", "N", &nrow, &nrow, &one, &C[0], &nrow, &Z[0],
+	      &nrow);
+
+    // C = t(Z) %*% Z
+    double zero = 0;
+    F77_DSYRK("U", "T", &nrow, &nrow, &one, &Z[0], &nrow, &zero, &C[0], &nrow);
+
+    // Copy result back to argument x.
+    // Note that C contains only the upper triangle
+    for (int i = 0; i < nrow; ++i) {
+	for (int j = 0; j <= i; ++j) {
+	    x[i * nrow + j] = x[j * nrow + i] = C[i * nrow + j];
+	}
+    }
+
+    /************* DEBUGGIN ****************/
+    for (int i = 0; i < nrow; i++) {
+	double const *Ztrans_i = &Ztrans[nrow * i];
+	for (int j = 0; j <= i; j++) {
+	    double const *Ztrans_j = &Ztrans[nrow * j];
+	    double xx = 0;
+	    for (int l = 0; l < nrow; l++) {
+		xx += Ztrans_i[l] * Ztrans_j[l];
+	    }
+	    if (abs(xx - x[nrow * j + i]) > 1e-3)
+		jags::throwLogicError("DEBUG: sampleWishart mismatch 2");
+	}
+    }
+    /**************** END DEBUGGIN **************/
 }
 
 
