6030ebc041a1fa8323ab21db962c53e33350f9df
martyn_plummer <martyn_plummer@sourceforge.net>
2017-06-18 23:40:56 +0200

    Documentation fixes

--- src/modules/bugs/functions/Sum.h
+++ src/modules/bugs/functions/Sum.h
@@ -11,7 +11,7 @@ namespace bugs {
      * Sum calculates the sum of the elements of an array
      * @see Mean
      * <pre>
-     * y <- sum(x[])
+     * y <- sum(x1[], x2[], ...)
      * </pre>
      */
     class Sum : public ScalarVectorFunction
